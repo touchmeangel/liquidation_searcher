9b3b2af436036ac463ab154dd96cf834641bb732
touchmeangel <vodkavoid@proton.me>
2026-02-01 11:48:27 +0200

    +worker

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -2,19 +2,13 @@ mod config;
 
 use config::Config;
 use connections::Redis;
+use protocols::marginfi::Marginfi;
 
 #[tokio::main]
 async fn main() {
-  let result: anyhow::Result<()> = async move {
-    let config = Config::open().await?;
+  let config = Config::open().unwrap();
 
-    let mut redis = Redis::new(&config.redis_url).await?;
-    
-    let accounts = redis.get_all().await?;
-    println!("checking {} accounts", accounts.len());
-
-    Ok(())
-  }.await;
+  let result = liquidate(config).await;
 
   if let Err(err) = result {
     eprintln!("error: {err}");
@@ -23,4 +17,27 @@ async fn main() {
         .skip(1)
         .for_each(|cause| eprintln!("caused by:\n  {cause}"));
   }
+}
+
+async fn liquidate(config: Config) -> anyhow::Result<()> {
+  let mut redis = Redis::new(&config.redis_url).await?;
+  let marginfi = Marginfi::new(config.http_url, config.ws_url).await?;
+  
+  let mut accounts = redis.get_all().await?;
+  let mut batches: Vec<Vec<_>> = Vec::new();
+  while !accounts.is_empty() {
+    let take = accounts.drain(..config.accounts_batch_size.min(accounts.len())).collect();
+    batches.push(take);
+  }
+
+  for accounts_batch in batches {
+    let results = marginfi.load_users(&accounts_batch).await?;
+    for result in results {
+
+    }
+
+    println!("checking {} accounts", accounts.len());
+  }
+
+  Ok(())
 }
